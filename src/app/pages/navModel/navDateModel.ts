aaab7c60925abc7d4fe4e5bbe24e225cd055c710
BrunoAlcantara <brunodeoliveiraalcantara@gmail.com>
2025-04-06 00:55:56 -0300

    Corrigimos o clique na <tr> do p-table: O ID agora está sendo passado corretamente para a função editarFornecedor(id) no componente.
    dição  abertura do modal com MatDialog passando dados via data do componente read para o componente update.
    
    Correção de duplicidade de envento
    
    Correção do visual do imput ao carregar os dados no componente  update

--- src/app/pages/navModel/navDateModel.ts
+++
@@ -1,5 +0,0 @@
-export interface naveDate {
-  title: string,
-  icon: string;
-  routeURL:string;
-}
--- src/app/types/registerResponse.type.ts
+++
@@ -1,5 +0,0 @@
-export type RegisterResponse = {
-  name: string,
-  email: string,
-  password: string
-}
